3b8da03131082c089dbc00b4e32e314835d35a11
Manuel Peyon <57277595+PeyonManuel@users.noreply.github.com>
2021-07-07 18:20:17 -0300

    Update ProjectsInfo.js

--- src/ProjectsInfo.js
+++ src/ProjectsInfo.js
@@ -34,7 +34,7 @@ export const projectsInfo = [
     code: 'https://github.com/PeyonManuel/mercado-libre-clone',
   },
   {
-    name: 'Mercado libre clone',
+    name: 'Movie project',
     technologies: ['React', 'Javascript', 'HTML & CSS'],
     about:
       'Directorio de películas utilizando The movie database API.\n El objetivo fue crear un directorio utilizando las funcionalidades más importantes de la API y hacer un diseño propio utilizando inspiraciones en otros directorios de películas populares.',
